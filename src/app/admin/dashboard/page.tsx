b914a8da470390af0dcdbb32d60c66a42e4fb8da
Dell <Dell@DESKTOP-KTCDR21>
2026-02-17 13:23:22 +0500

    remove authenation in admin dashbaord

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import { EcommerceMetrics } from "@/components/ecommerce/EcommerceMetrics";
 import MonthlyTarget from "@/components/ecommerce/MonthlyTarget";
 import MonthlySalesChart from "@/components/ecommerce/MonthlySalesChart";
@@ -7,7 +8,6 @@ import { AdminAuth } from '@/hooks/AdminAuth';
 
 
 export default function Ecommerce() {
-   AdminAuth();
   return (
     <div className="grid grid-cols-12 gap-4 md:gap-6">
       <div className="col-span-12 space-y-6 xl:col-span-7">
